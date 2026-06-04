f5ff1459b4e3f8bb5229efb032a5b450dbfc65db
afiyakouser07 <afiyakouser07@gmail.com>
2026-06-04 11:49:13 +0530

    Create 1_Advanced_Analysis.py

---
+++ pages/1_Advanced_Analysis.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+from utils.data_loader import load_data
+from utils.plots import *
+
+df = load_data()
+
+st.title("📊 Advanced Analysis")
+
+st.plotly_chart(
+    study_vs_score(df),
+    use_container_width=True
+)
+
+st.plotly_chart(
+    attendance_vs_score(df),
+    use_container_width=True
+)
+
+st.plotly_chart(
+    sleep_vs_score(df),
+    use_container_width=True
+)
